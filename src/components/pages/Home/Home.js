1dc038d7e13cb7741164e9488da4db8919a2052c
Napstyyy <mate2003-4@outlook.com>
2024-08-12 17:09:54 -0500

    carrusel scrolleable pero con animacion a la izquierda rara

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,10 +3,15 @@ import "./styles/Home.css";
 import mockData from "../../../data/mockData.json";
 import { RxDashboard } from "react-icons/rx";
 import NotificationIcon from "../../../assets/images/Home/notificationIcon.png";
+import { BsArrowRightCircle } from "react-icons/bs";
+import MessageIcon from "../../../assets/images/Home/Message.png";
+import MessageNIcon from "../../../assets/images/Home/MessageN.png";
+import { IoIosArrowForward } from "react-icons/io";
 
 const HomeView = () => {
   const options = { weekday: "long", day: "numeric" };
   const malaysiaTime = new Date().toLocaleDateString("en-MY", options);
+  let hasAnyMessage = false;
 
   return (
     <div className="Home">
@@ -18,7 +23,7 @@ const HomeView = () => {
         <button className="home-button right">
           <img
             src={NotificationIcon}
-            alt="Noty"
+            alt="Notification"
             className="notification-image"
           />
         </button>
@@ -30,39 +35,81 @@ const HomeView = () => {
             🙌
           </span>
         </h1>
-        <div class="participated-project">
-          <h2>Participated Project</h2>
-          <p>Project you have quoted or chat with</p>
-          <div class="project-details">
-            <div class="project-images">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="Project 1"
-                class="project-image"
-              />
-              <img
-                src="https://via.placeholder.com/50"
-                alt="Project 2"
-                class="project-image"
-              />
-              <img
-                src="https://via.placeholder.com/50"
-                alt="Project 3"
-                class="project-image"
-              />
-            </div>
-            <button class="view-projects">View All Projects</button>
-          </div>
+      
+        <div className="carousel-container">
+  <div className="carousel">
+    <div className="participated-project">
+      <h2>Participated Project</h2>
+      <p>Projects you have quoted or chatted with</p>
+      <div className="project-details">
+        <div className="project-images">
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 1"
+            className="project-image"
+          />
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 2"
+            className="project-image"
+          />
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 3"
+            className="project-image"
+          />
         </div>
+        <button className="view-projects">View All Projects</button>
+      </div>
+    </div>
+
+    <div className="participated-project participated-project-1">
+      <h2>Not Joined Project</h2>
+      <p>Projects you joined but closed</p>
+      <div className="project-details-1">
+        <div className="project-images">
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 1"
+            className="project-image"
+          />
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 2"
+            className="project-image"
+          />
+          <img
+            src="https://via.placeholder.com/50"
+            alt="Project 3"
+            className="project-image"
+          />
+        </div>
+        <button className="view-projects">Progress</button>
+      </div>
+    </div>
+  </div>
+</div>
 
         <div className="all-projects">
-          <h2>All Projects</h2>
+          <h2>
+            All Projects <IoIosArrowForward  className="all-projects-icon" />
+          </h2>
           {mockData.projects.map((project, index) => (
             <div className="project-card" key={index}>
-              <p>{project.quotations} quotation uploaded</p>
+              <p>{project.quotations} quotation(s) uploaded</p>
               <h3>{project.title}</h3>
               <p>Average price @ {project.averagePrice}</p>
-              {project.chatIcon && <img src="chat-icon.png" alt="chat" />}
+              {project.HasMessages ? (
+                <>
+                  <img src={MessageNIcon} alt="Message Notification" />
+                  {hasAnyMessage = true}
+                </>
+              ) : (
+                <>
+                  <img src={MessageIcon} alt="Message" />
+                  {hasAnyMessage = false}
+                </>
+              )}
             </div>
           ))}
         </div>
